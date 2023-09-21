0fa2e1fc089ef44af1e7a369933a86a705d7d938
CreamyOmlette <al1g18@yandex.com>
2023-09-21 09:12:41 +0100

    vlad fix

--- pages/_document.js
+++ pages/_document.js
@@ -3,15 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *;**script-src 'self' http://onlineerp.solution.quebec 'unsafe-inline' 'unsafe-eval';** "
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
-      <meta
-        httpEquiv="Content-Security-Policy"
-        content="default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *;**script-src 'self' http://onlineerp.solution.quebec 'unsafe-inline' 'unsafe-eval';** "
-      />
     </Html>
   );
 }
