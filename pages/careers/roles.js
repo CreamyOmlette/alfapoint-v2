bfb96d412942d1e324b2dd0a69cea11f3c3f04ce
CreamyOmlette <al1g18@yandex.com>
2023-09-22 09:16:05 +0100

    moved roles to another file

---
+++ pages/careers/roles.js
@@ -0,0 +1,137 @@
+export const roles = [
+  {
+    title: "QA Automation (Mid or Senior)",
+    location: "Engineer	Germany, European Union",
+    technologies:
+      "Kubernetes, RabbitMQ, Docker, AWS, Azure, Terraform, Jenkins, Git, Ansible",
+    requirements: () => (
+      <>
+        <p className="pb-[50px]">
+          - Bachelor&apos;s degree in Computer Science or equivalent <br /> -
+          Web services (REST) <br /> - Web technologies (Angular2, Typescript
+          and HTML5) - Linux knowledge (scripting, configuration) <br /> -
+          Excellent knowledge of object-oriented programming (OOP). <br /> -
+          Experience in version control is preferred <br />- Strong knowledge of
+          Javascript, HTML, CSS and related tools / packages
+          <br /> - Solid knowledge of relational databases and / or
+          non-relational databases
+          <br /> - Good understanding of software architecture and design
+          <br /> - Advanced level of written and spoken English
+        </p>
+        <h2 className="pb-[25px]">Knowing the following is a plus:</h2>
+        <p className="pb-[50px]">
+          - C++ development skills
+          <br />- Knowledge of scripting languages- NodeJS, Express, Npm,
+          Angular
+          <br /> - Cloud architecture (AWS / Azure / Google Cloud) - ReactJS /
+          React Native / GraphQL
+          <br /> - Routing, TCP / IP, sockets- Database knowledge (SQL)
+          <br /> - NoSQL knowledge
+          <br /> - Familiarity with Python / Perl / Ruby would be a plus
+          <br /> - Experience in working with V-Model, Agile methods
+        </p>
+        <h2 className="pb-[25px]">Responsibilities</h2>
+        <p className="pb-[25px]">
+          Continuous development, continuous integration and continuous testing
+          using GIT, Jenkins Configuration management and continuous deployment
+          using Chef, SaltStack, Ansible and Docker.
+        </p>
+        <p className="pb-[25px]">You will find out more at the interview.</p>
+        <p>
+          To apply please send your CV to office@alfa-point.com or contact us by
+          any other means possible.
+        </p>
+      </>
+    ),
+  },
+  {
+    title: "QA Automation (Junior)",
+    location: "Engineer	Germany, European Union",
+    technologies:
+      "Kubernetes, RabbitMQ, Docker, AWS, Azure, Terraform, Jenkins, Git, Ansible",
+    requirements: () => (
+      <>
+        <p className="pb-[50px]">
+          - Bachelor&apos;s degree in Computer Science or equivalent <br /> -
+          Web services (REST) <br /> - Web technologies (Angular2, Typescript
+          and HTML5) - Linux knowledge (scripting, configuration) <br /> -
+          Excellent knowledge of object-oriented programming (OOP). <br /> -
+          Experience in version control is preferred <br />- Strong knowledge of
+          Javascript, HTML, CSS and related tools / packages
+          <br /> - Solid knowledge of relational databases and / or
+          non-relational databases
+          <br /> - Good understanding of software architecture and design
+          <br /> - Advanced level of written and spoken English
+        </p>
+        <h2 className="pb-[25px]">Knowing the following is a plus:</h2>
+        <p className="pb-[50px]">
+          - C++ development skills
+          <br />- Knowledge of scripting languages- NodeJS, Express, Npm,
+          Angular
+          <br /> - Cloud architecture (AWS / Azure / Google Cloud) - ReactJS /
+          React Native / GraphQL
+          <br /> - Routing, TCP / IP, sockets- Database knowledge (SQL)
+          <br /> - NoSQL knowledge
+          <br /> - Familiarity with Python / Perl / Ruby would be a plus
+          <br /> - Experience in working with V-Model, Agile methods
+        </p>
+        <h2 className="pb-[25px]">Responsibilities</h2>
+        <p className="pb-[25px]">
+          Continuous development, continuous integration and continuous testing
+          using GIT, Jenkins Configuration management and continuous deployment
+          using Chef, SaltStack, Ansible and Docker.
+        </p>
+        <p className="pb-[25px]">You will find out more at the interview.</p>
+        <p>
+          To apply please send your CV to office@alfa-point.com or contact us by
+          any other means possible.
+        </p>
+      </>
+    ),
+  },
+  {
+    title: "QA Automation (Junior) debil",
+    location: "Engineer	Germany, European Union",
+    technologies:
+      "Kubernetes, RabbitMQ, Docker, AWS, Azure, Terraform, Jenkins, Git, Ansible",
+    requirements: () => (
+      <>
+        <p className="pb-[50px]">
+          - Bachelor&apos;s degree in Computer Science or equivalent <br /> -
+          Web services (REST) <br /> - Web technologies (Angular2, Typescript
+          and HTML5) - Linux knowledge (scripting, configuration) <br /> -
+          Excellent knowledge of object-oriented programming (OOP). <br /> -
+          Experience in version control is preferred <br />- Strong knowledge of
+          Javascript, HTML, CSS and related tools / packages
+          <br /> - Solid knowledge of relational databases and / or
+          non-relational databases
+          <br /> - Good understanding of software architecture and design
+          <br /> - Advanced level of written and spoken English
+        </p>
+        <h2 className="pb-[25px]">Knowing the following is a plus:</h2>
+        <p className="pb-[50px]">
+          - C++ development skills
+          <br />- Knowledge of scripting languages- NodeJS, Express, Npm,
+          Angular
+          <br /> - Cloud architecture (AWS / Azure / Google Cloud) - ReactJS /
+          React Native / GraphQL
+          <br /> - Routing, TCP / IP, sockets- Database knowledge (SQL)
+          <br /> - NoSQL knowledge
+          <br /> - Familiarity with Python / Perl / Ruby would be a plus
+          <br /> - Experience in working with V-Model, Agile methods
+        </p>
+        <h2 className="pb-[25px]">Responsibilities</h2>
+        <p className="pb-[25px]">
+          Continuous development, continuous integration and continuous testing
+          using GIT, Jenkins Configuration management and continuous deployment
+          using Chef, SaltStack, Ansible and Docker.
+        </p>
+        <p className="pb-[25px]">You will find out more at the interview.</p>
+        <p>
+          To apply please send your CV to office@alfa-point.com or contact us by
+          any other means possible.
+        </p>
+      </>
+    ),
+  },
+];
