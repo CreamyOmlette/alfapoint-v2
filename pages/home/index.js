aca2a9a7f3d2043fd3f9d1d2cc585c4d32421351
CreamyOmlette <al1g18@yandex.com>
2023-09-22 10:51:26 +0100

    changed min h on home pages first block

--- pages/home/index.js
+++ pages/home/index.js
@@ -88,7 +88,7 @@ export default function Home() {
           open={isOpen}
         ></PopupModal>
       )}
-      <section className="min-h-[800px] h-[100vh] relative">
+      <section className="min-h-[650px] h-[100vh] relative">
         <div className={`w-[100%] h-[100%] absolute z-0`}>
           <video
             autoPlay
