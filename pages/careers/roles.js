1cf65368cdda9498e57423c181228816e036731d
alexlipceanu <al1g18@yandex.com>
2023-09-22 13:13:38 +0300

    Modified jobs and descriptions for the jobs

--- pages/careers/roles.js
+++ pages/careers/roles.js
@@ -1,137 +1,77 @@
 export const roles = [
   {
-    title: "QA Automation (Mid or Senior)",
-    location: "Engineer	Germany, European Union",
+    title: "PHP Backend Software Engineer",
+    location: "France, EU, Remote",
     technologies:
-      "Kubernetes, RabbitMQ, Docker, AWS, Azure, Terraform, Jenkins, Git, Ansible",
+      "PHP, Laravel, AWS, Git",
     requirements: () => (
-      <>
+      <>      
         <p className="pb-[50px]">
-          - Bachelor&apos;s degree in Computer Science or equivalent <br /> -
-          Web services (REST) <br /> - Web technologies (Angular2, Typescript
-          and HTML5) - Linux knowledge (scripting, configuration) <br /> -
-          Excellent knowledge of object-oriented programming (OOP). <br /> -
-          Experience in version control is preferred <br />- Strong knowledge of
-          Javascript, HTML, CSS and related tools / packages
-          <br /> - Solid knowledge of relational databases and / or
-          non-relational databases
-          <br /> - Good understanding of software architecture and design
-          <br /> - Advanced level of written and spoken English
+          - Bachelor&apos;s degree in Computer Science, Software Engineering, or a related field (or equivalent experience) is a plus, however is not mandatory. <br />
+          - Proven experience as a software engineer, with a focus on web application development using Laravel and PHP.<br />
+          - Strong proficiency in Laravel and PHP, with a deep understanding of their ecosystems.<br />
+          - Familiarity with front-end technologies (HTML, CSS, JavaScript) and their integration with Laravel.<br />
+          - Experience with relational databases, such as MySQL or PostgreSQL.<br />
+          - Knowledge of RESTful APIs and web services.<br />
+          - Solid understanding of software development best practices, including version control (e.g., Git) and agile methodologies.<br />
+          - Excellent problem-solving skills and attention to detail.<br/>
+          - Strong communication and teamwork skills.<br/>
+          - Self-motivated with the ability to work independently and meet project deadlines.<br/>
         </p>
-        <h2 className="pb-[25px]">Knowing the following is a plus:</h2>
-        <p className="pb-[50px]">
-          - C++ development skills
-          <br />- Knowledge of scripting languages- NodeJS, Express, Npm,
-          Angular
-          <br /> - Cloud architecture (AWS / Azure / Google Cloud) - ReactJS /
-          React Native / GraphQL
-          <br /> - Routing, TCP / IP, sockets- Database knowledge (SQL)
-          <br /> - NoSQL knowledge
-          <br /> - Familiarity with Python / Perl / Ruby would be a plus
-          <br /> - Experience in working with V-Model, Agile methods
-        </p>
-        <h2 className="pb-[25px]">Responsibilities</h2>
+        <h2 className="pb-[25px]">Responsibilities:</h2>
         <p className="pb-[25px]">
-          Continuous development, continuous integration and continuous testing
-          using GIT, Jenkins Configuration management and continuous deployment
-          using Chef, SaltStack, Ansible and Docker.
+          - Application Development: Develop and maintain high-quality web applications using Laravel and PHP, ensuring they meet performance, security, and scalability requirements.<br/>
+          - Code Review: Participate in code reviews to maintain code quality, share knowledge, and ensure adherence to coding standards.<br/>
+          - Collaboration: Collaborate with cross-functional teams, including front-end developers, designers, and product managers, to deliver exceptional software solutions.<br/>
+          - Problem Solving: Troubleshoot and resolve software defects and issues, providing efficient and effective solutions.<br/>
+          - Performance Optimization: Identify and address performance bottlenecks, ensuring optimal application speed and responsiveness.<br/>
+          - Security: Implement security best practices and measures to protect applications against vulnerabilities and threats.<br/>
+          - Documentation: Maintain clear and concise documentation for code, processes, and systems.<br/>
         </p>
-        <p className="pb-[25px]">You will find out more at the interview.</p>
+        <p className="pb-[25px]">If you are a passionate Medium-Senior Software Engineer with expertise in Laravel/PHP and are ready to contribute to exciting projects, we encourage you to apply and join our team at Alfapoint. Together, we can build innovative solutions that make a real impact in the world of technology.</p>
         <p>
-          To apply please send your CV to office@alfa-point.com or contact us by
+          To apply please send your CV to <a className="text-regal-blue">office@alfa-point.com</a> or contact us by
           any other means possible.
         </p>
       </>
     ),
   },
   {
-    title: "QA Automation (Junior)",
-    location: "Engineer	Germany, European Union",
+    title: ".NET Software Engineer",
+    location: "Germany, EU, Remote",
     technologies:
-      "Kubernetes, RabbitMQ, Docker, AWS, Azure, Terraform, Jenkins, Git, Ansible",
+      "C#, ASP.NET, and .NET Core, HTML, CSS, JavaScript, RESTful APIs",
     requirements: () => (
-      <>
+      <>      
         <p className="pb-[50px]">
-          - Bachelor&apos;s degree in Computer Science or equivalent <br /> -
-          Web services (REST) <br /> - Web technologies (Angular2, Typescript
-          and HTML5) - Linux knowledge (scripting, configuration) <br /> -
-          Excellent knowledge of object-oriented programming (OOP). <br /> -
-          Experience in version control is preferred <br />- Strong knowledge of
-          Javascript, HTML, CSS and related tools / packages
-          <br /> - Solid knowledge of relational databases and / or
-          non-relational databases
-          <br /> - Good understanding of software architecture and design
-          <br /> - Advanced level of written and spoken English
+          - Bachelor&apos;s degree in Computer Science, Software Engineering, or a related field (or equivalent experience) is a plus, however is not mandatory. <br />
+          - Demonstrated experience as a software engineer with a primary focus on developing applications using the .NET stack.<br />
+          - Proficiency in the entire .NET ecosystem, including C#, ASP.NET, and .NET Core.<br />
+          - Experience with relational databases, such as SQL Server or Oracle.<br />
+          - Knowledge of web development technologies (HTML, CSS, JavaScript) and their integration with .NET.<br />
+          - Knowledge of RESTful APIs and web services.<br />
+          - Strong grasp of software development best practices, including version control (e.g., Git) and agile methodologies.<br />
+          - Exceptional problem-solving skills and attention to detail.<br/>
+          - Effective communication and teamwork abilities.<br/>
+          - Self-motivated and capable of working independently while meeting project deadlines.<br/>
         </p>
-        <h2 className="pb-[25px]">Knowing the following is a plus:</h2>
-        <p className="pb-[50px]">
-          - C++ development skills
-          <br />- Knowledge of scripting languages- NodeJS, Express, Npm,
-          Angular
-          <br /> - Cloud architecture (AWS / Azure / Google Cloud) - ReactJS /
-          React Native / GraphQL
-          <br /> - Routing, TCP / IP, sockets- Database knowledge (SQL)
-          <br /> - NoSQL knowledge
-          <br /> - Familiarity with Python / Perl / Ruby would be a plus
-          <br /> - Experience in working with V-Model, Agile methods
-        </p>
-        <h2 className="pb-[25px]">Responsibilities</h2>
+        <h2 className="pb-[25px]">Responsibilities:</h2>
         <p className="pb-[25px]">
-          Continuous development, continuous integration and continuous testing
-          using GIT, Jenkins Configuration management and continuous deployment
-          using Chef, SaltStack, Ansible and Docker.
+          - Software Development: Design, build, and maintain high-quality software applications using the .NET stack while ensuring they meet performance, security, and scalability standards.<br/>
+          - Code Review: Participate in code reviews to uphold code quality, promote knowledge sharing, and ensure compliance with coding standards.<br/>
+          - Team Collaboration: Collaborate closely with cross-functional teams, including front-end developers, UX/UI designers, and product managers, to deliver exceptional software solutions.<br/>
+          - Problem Solving: Identify and resolve software defects and issues, offering efficient and effective solutions.<br/>
+          - Performance Optimization: Recognize and address performance bottlenecks, optimizing application speed and responsiveness.<br/>
+          - Security: Implement best practices and security measures to safeguard applications against potential vulnerabilities and threats.<br/>
+          - Documentation: Maintain clear and concise documentation for code, processes, and systems.<br/>
         </p>
-        <p className="pb-[25px]">You will find out more at the interview.</p>
+        <p className="pb-[25px]">If you are a passionate Medium-Senior Software Engineer with expertise in Laravel/PHP and are ready to contribute to exciting projects, we encourage you to apply and join our team at Alfapoint. Together, we can build innovative solutions that make a real impact in the world of technology.</p>
         <p>
-          To apply please send your CV to office@alfa-point.com or contact us by
+          To apply please send your CV to <a className="text-regal-blue">office@alfa-point.com</a> or contact us by
           any other means possible.
         </p>
       </>
     ),
   },
-  {
-    title: "QA Automation (Junior) debil",
-    location: "Engineer	Germany, European Union",
-    technologies:
-      "Kubernetes, RabbitMQ, Docker, AWS, Azure, Terraform, Jenkins, Git, Ansible",
-    requirements: () => (
-      <>
-        <p className="pb-[50px]">
-          - Bachelor&apos;s degree in Computer Science or equivalent <br /> -
-          Web services (REST) <br /> - Web technologies (Angular2, Typescript
-          and HTML5) - Linux knowledge (scripting, configuration) <br /> -
-          Excellent knowledge of object-oriented programming (OOP). <br /> -
-          Experience in version control is preferred <br />- Strong knowledge of
-          Javascript, HTML, CSS and related tools / packages
-          <br /> - Solid knowledge of relational databases and / or
-          non-relational databases
-          <br /> - Good understanding of software architecture and design
-          <br /> - Advanced level of written and spoken English
-        </p>
-        <h2 className="pb-[25px]">Knowing the following is a plus:</h2>
-        <p className="pb-[50px]">
-          - C++ development skills
-          <br />- Knowledge of scripting languages- NodeJS, Express, Npm,
-          Angular
-          <br /> - Cloud architecture (AWS / Azure / Google Cloud) - ReactJS /
-          React Native / GraphQL
-          <br /> - Routing, TCP / IP, sockets- Database knowledge (SQL)
-          <br /> - NoSQL knowledge
-          <br /> - Familiarity with Python / Perl / Ruby would be a plus
-          <br /> - Experience in working with V-Model, Agile methods
-        </p>
-        <h2 className="pb-[25px]">Responsibilities</h2>
-        <p className="pb-[25px]">
-          Continuous development, continuous integration and continuous testing
-          using GIT, Jenkins Configuration management and continuous deployment
-          using Chef, SaltStack, Ansible and Docker.
-        </p>
-        <p className="pb-[25px]">You will find out more at the interview.</p>
-        <p>
-          To apply please send your CV to office@alfa-point.com or contact us by
-          any other means possible.
-        </p>
-      </>
-    ),
-  },
+  
 ];
